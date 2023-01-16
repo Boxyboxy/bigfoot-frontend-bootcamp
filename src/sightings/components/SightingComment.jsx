f5dc35c675abd2dfb406f334490086bbdc9d4e2e
boxyboxy <eugenengboxiang@gmail.com>
2023-01-16 13:20:13 +0800

    Edit functionality completed

---
+++ src/sightings/components/SightingComment.jsx
@@ -0,0 +1,98 @@
+import { Comment } from '@ant-design/compatible';
+import { Form, Space, Tooltip, Input } from 'antd';
+import { CloseOutlined, DeleteOutlined, EditOutlined, SaveOutlined } from '@ant-design/icons';
+import { formatDistance, format } from 'date-fns';
+import { useCallback, useState, useEffect } from 'react';
+import { useNotification } from '../../common/hooks/useNotification';
+import { httpClient } from '../../common/httpClient';
+
+export function SightingComment({ comment, comments, setComments, commentIndex }) {
+  const { notify, notifyContext } = useNotification();
+  const [isEditing, setIsEditing] = useState(false);
+  const [form] = Form.useForm();
+
+  const deleteComment = useCallback(
+    (commentId, commentIndex) => {
+      httpClient.delete(`/comments/${commentId}`).then(() => {
+        notify('success', 'Deleted comment');
+        const newComments = [...comments];
+        console.log(newComments);
+        newComments.splice(commentIndex, 1);
+        setComments(newComments);
+      });
+    },
+    [comments]
+  );
+
+  const updateComment = useCallback(() => {
+    httpClient
+      .patch(`/comments/${comment.id}`, {
+        content: form.getFieldValue('comment_content')
+      })
+      .then(() => {
+        notify('success', 'Updated comment');
+        const newComments = [...comments];
+        newComments[commentIndex].content = form.getFieldValue('comment_content');
+        setIsEditing(false);
+      });
+    console.log(form.getFieldsValue());
+  });
+  useEffect(() => {
+    form.setFieldValue('comment_content', comment.content);
+  }, [comment, form]);
+
+  return (
+    <>
+      {notifyContext}
+      <Comment
+        actions={
+          isEditing
+            ? [
+                <span key="update-confirm" onClick={updateComment}>
+                  <Space>
+                    <SaveOutlined /> Save
+                  </Space>
+                </span>,
+                <span key="update-cancel" onClick={() => setIsEditing(false)}>
+                  <Space>
+                    <CloseOutlined /> Cancel
+                  </Space>
+                </span>
+              ]
+            : [
+                <span key="comment-edit" onClick={() => setIsEditing(true)}>
+                  <Space>
+                    <EditOutlined /> Edit
+                  </Space>
+                </span>,
+
+                <span key="comment-delete" onClick={() => deleteComment(comment.id, commentIndex)}>
+                  <Space>
+                    <DeleteOutlined /> Delete
+                  </Space>
+                </span>
+              ]
+        }
+        key={comment.id}
+        content={
+          <Form form={form} onSubmitCapture={updateComment}>
+            {isEditing ? (
+              <Form.Item name="comment_content" style={{ marginBottom: 0 }}>
+                <Input />
+              </Form.Item>
+            ) : (
+              comment.content
+            )}
+          </Form>
+        }
+        datetime={
+          <Tooltip title={format(new Date(comment.createdAt), 'd MMM yy h.mm a')}>
+            <span>
+              {formatDistance(new Date(comment.createdAt), new Date(), { addSuffix: true })}{' '}
+            </span>
+          </Tooltip>
+        }
+      />
+    </>
+  );
+}
--- src/sightings/components/SightingComments.jsx
+++ src/sightings/components/SightingComments.jsx
@@ -1,10 +1,9 @@
-import { Comment } from '@ant-design/compatible';
-import { formatDistance, format } from 'date-fns';
-import { Button, Form, Input, Space, Tooltip } from 'antd';
+import { Button, Form, Input } from 'antd';
 import { useCallback } from 'react';
 import { httpClient } from '../../common/httpClient';
 import { useNotification } from '../../common/hooks/useNotification';
-import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
+
+import { SightingComment } from './SightingComment';
 
 export function SightingComments({ comments, sightingId, setComments }) {
   const [form] = Form.useForm();
@@ -21,19 +20,6 @@ export function SightingComments({ comments, sightingId, setComments }) {
       });
   }, [sightingId, comments]);
 
-  const deleteComment = useCallback(
-    (commentId, commentIndex) => {
-      httpClient.delete(`/comments/${commentId}`).then(() => {
-        notify('success', 'Deleted comment');
-        const newComments = [...comments];
-        console.log(newComments);
-        newComments.splice(commentIndex, 1);
-        setComments(newComments);
-      });
-    },
-    [comments]
-  );
-
   return (
     <>
       <Form form={form} layout="inline" onSubmitCapture={createComment}>
@@ -46,29 +32,12 @@ export function SightingComments({ comments, sightingId, setComments }) {
         </Button>
       </Form>
       {comments.map((comment, index) => (
-        <Comment
-          actions={[
-            <span key="comment-edit">
-              <Space>
-                <EditOutlined /> Edit
-              </Space>
-            </span>,
-
-            <span key="comment-delete" onClick={() => deleteComment(comment.id, index)}>
-              <Space>
-                <DeleteOutlined /> Delete
-              </Space>
-            </span>
-          ]}
+        <SightingComment
           key={comment.id}
-          content={comment.content}
-          datetime={
-            <Tooltip title={format(new Date(comment.createdAt), 'd MMM yy h.mm a')}>
-              <span>
-                {formatDistance(new Date(comment.createdAt), new Date(), { addSuffix: true })}{' '}
-              </span>
-            </Tooltip>
-          }
+          comment={comment}
+          commentIndex={index}
+          comments={comments}
+          setComments={setComments}
         />
       ))}
     </>
